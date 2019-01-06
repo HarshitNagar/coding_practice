d07a76b8b758148be05a1f8c87a987b294fb8c20
HarshitNagar <hnagar.000@gmail.com>
2019-01-07 00:26:01 +0530

    added json depth finder

---
+++ techsetgo2019/json-depth.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stack>
+#include<string.h>
+using namespace std;
+
+int json_depth(char* jsonString)
+{
+	int depth=0;	
+	stack<char>s;
+	for(int i=0; i<strlen(jsonString); i++)
+	{
+		if(*(jsonString+i)=='{')
+			s.push('{');
+		else if(*(jsonString+i) == '}')
+		{
+			if(s.empty()==1)
+				return -1;
+			else if(s.top()=='{')
+			{
+				depth++;
+				s.pop();
+			}
+
+
+		}
+	}
+	//cout<<s.top()<<endl<<depth<<endl;
+	if(s.empty()==1)
+		return depth;
+	else 
+		return -1;
+
+
+	return depth;
+}
+
+int main()
+{
+	char jsonString1[]="{aaa{s{sad}fdf}gfs}";
+	char jsonString2[]="";
+	char jsonString3[]="{";
+	char jsonString4[]="}";
+	cout<<json_depth(jsonString1)<<endl;
+	cout<<json_depth(jsonString2)<<endl;
+	cout<<json_depth(jsonString3)<<endl;
+	cout<<json_depth(jsonString4)<<endl;
+	return 0;
+}
